0f8a06c3a80eae1ec8ae355d5ab9e57462030ddd
yhmmm <126881621+yhmmm@users.noreply.github.com>
2023-12-10 21:09:33 +0800

    Add files via upload

---
+++ 树状数组.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int n,a[1000001],ca[1000001],cb[1000001],t;
+int lowbit(int X)
+{
+	return X&(-X);
+}
+void update(int idx,int x)
+{
+	for(int i=idx;i<=n;i+=lowbit(i))
+	{
+		ca[i]+=x,cb[i]+=x*idx;
+	}
+}
+int sum(int v)
+{
+	t=0;
+	for(int i=v;i>0;i-=lowbit(i))
+	{
+		t+=(v+1)*ca[i]-cb[i];
+	}
+	return t;
+}
