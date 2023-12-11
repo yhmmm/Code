38cd3d1ada3050c97a5728238c38bf27e9dd68ce
yhmm <126881621+yhmmm@users.noreply.github.com>
2023-12-11 20:58:35 +0800

    kruskal.cpp

--- kruskal.cpp
+++ kruskal.cpp
@@ -26,14 +26,14 @@ bool judge(int a,int b)
 	return find(a)==find(b);
 }
 int main(){
-    cin>>n>>m;
+	cin>>n>>m;
 	for(int i=1;i<=n;i++)
 	{
 		f[i]=i;
 	}
 	for(int i=1;i<=m;i++)
 	{
-        cin>>a[i].u>>a[i].v>>a[i].w;
+		cin>>a[i].u>>a[i].v>>a[i].w;
 	}
 	sort(a+1,a+m+1);
 	for(int i=1;i<=m;i++)
