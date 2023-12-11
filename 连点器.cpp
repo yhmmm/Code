318bae32c8dc450e9f0191a6ad71ac7133081589
yhmm <126881621+yhmmm@users.noreply.github.com>
2023-12-11 20:55:42 +0800

    连点器.cpp

--- 连点器.cpp
+++ 连点器.cpp
@@ -2,20 +2,20 @@
 #include<windows.h>
 using namespace std;
 int main(){
-    while(1)
-    {
-    	if(GetAsyncKeyState(VK_F2))//F2
-    	{
-    		while(1)
+	while(1)
+	{
+		if(GetAsyncKeyState(VK_F2))//F2
+		{
+			while(1)
 			{
-                mouse_event(MOUSEEVENTF_LEFTDOWN|MOUSEEVENTF_LEFTUP,0,0,0,0);
-                Sleep(10);
-                if(GetAsyncKeyState(VK_F4))//F4
+				mouse_event(MOUSEEVENTF_LEFTDOWN|MOUSEEVENTF_LEFTUP,0,0,0,0);
+				Sleep(10);
+				if(GetAsyncKeyState(VK_F4))//F4
 				{
 					break;
 				}
-            }
+			}
 		}
 	}
-    return 0;
+	return 0;
 }
